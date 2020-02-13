88a5b25331ef8360ce31d53ff6543fec314b23f7
codyvi <davidcantumartinez@gmail.com>
2020-02-12 20:40:33 -0600

    Finished the programs

--- Code/Fibo.c
+++ Code/Fibo.c
@@ -4,7 +4,6 @@ void fibonacci(int n)
 {
   int i = 0;
 	int j = 1;
-  
   if(n == 1)
   {
     printf("%d", i);
@@ -30,11 +29,9 @@ void fibonacci(int n)
 
 int main()
 {
- 
   int Num = 0;
   printf("Dame la cantidad de numeros de la secuencia: ");
   scanf("%d", &Num);
-
   printf("La secuencia fibonnaci de ");
   printf("%d", Num);
   if(Num == 1)
@@ -45,10 +42,7 @@ int main()
   {
     printf(" numeros es: ");
   }
-  
   fibonacci(Num);
-
   printf("\n");
-
   return 0;
 }
--- Code/Prime.c
+++ Code/Prime.c
@@ -24,16 +24,13 @@ int main()
   int tmp = 0;
   printf("Dame un numero: ");
   scanf("%d", &tmp);
-
   if(isPrime(tmp))
   {
     printf("Es un numero primo\n");
   }
-
   else
   {
     printf("No es un numero primo\n");
   }
-
   return 0;
 }
