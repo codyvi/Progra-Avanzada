31ca7dc88dbdd824af8139a30692f2a5fb9993bb
David <davidcantumartinez@gmail.com>
2020-02-19 11:34:06 -0600

    Tarea 4 Finished

--- Code/ArraysAndMatrixs.c
+++ Code/ArraysAndMatrixs.c
@@ -5,45 +5,109 @@
 //las dos matrices, la suma, resta y multiplicación de cada una
 
 #include "stdio.h"
+#define size 2
 
-void printMatrices(int iMatrixA[2][2]){
+//Imprime las matrices
+void printMat(int iMat[size][size]){
     printf("La matriz es: \n");
-    for(int i = 0; i < 2; i++){
-        for(int j = 0; j < 2; j++){
-            printf("%d\t", iMatrixA[i][j]);   // Imprimir un renglon
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = 0; j < size; j++)
+        {
+            //Imprime cada valor de la matriz 
+            printf("%d\t", iMat[i][j]);   
         }
-        printf("\n"); // Cambiar de renglon
+        //Cambia de renglón
+        printf("\n"); 
     }
 }
 
+//Suma dos matrices y luego despliega el resultado
+void SumaMat(int iMatA[size][size], int iMatB[size][size])
+{
+    int iMatAux[size][size];
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = 0; j < size; j++)
+        {
+            iMatAux[i][j] = iMatA[i][j] + iMatB[i][j]; //Suma las matrices
+        }
+    }
+    printf("---Suma de dos matrices---\n");
+    printMat(iMatAux); //Llama a printMat para desplegar el resultado
 
+}
 
-//Función Principal 
+//Resta dos matrices y luego despliega el resultado
+void RestaMat(int iMatA[size][size], int iMatB[size][size])
+{
+    int iMatAux[size][size];
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = 0; j < size; j++)
+        {
+            iMatAux[i][j] = iMatA[i][j] - iMatB[i][j]; //Resta las matrices
+        }
+    }
+    printf("---Resta de dos matrices---\n"); 
+    printMat(iMatAux); //Llama a printMat para desplegar el resultado
+}
+
+//Multiplica dos matrices, y luego despliega el resultado
+void MultMat(int iMatA[size][size], int iMatB[size][size])
+{
+    int iMatAux[size][size];
+    int iAux = 0;
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = 0; j < size; j++)
+        {
+            for(int k = 0; k < size; k++)
+            {
+                iAux +=  iMatA[i][k]*iMatB[k][j]; //Multiplicando las matrices
+            }
+            iMatAux[i][j] = iAux; //Asignando valor aux a la matriz a desplegar
+            iAux = 0;
+        }
+    }
+    printf("---Multiplicacion de dos matrices---\n"); 
+    printMat(iMatAux); //Llama a printMat para desplegar el resultado
+}
+
+//Función Principal
 int main()
 {
     //Varaibles a usar para matrices  
-    int iMatOne[2][2];
-    int iMatTwo[2][2];
+    int iMatOne[size][size];
+    int iMatTwo[size][size];
 
     
-
-    
-    printf("Introduce los 4 elementos de la primera matriz: \n");
-    for(int i = 0; i < 2; i++){
-        for(int j = 0; j < 2; j++){
+    printf("Dame los 4 elementos de la matriz uno: \n");
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = 0; j < size; j++)
+        {
             scanf("%d", &iMatOne[i][j]);
         }
     }
 
-    printf("Introduce los 4 elementos de la segunda matriz: \n");
-    for(int i = 0; i < 2; i++){
-        for(int j = 0; j < 2; j++){
+    printf("Dame los 4 elementos de la matriz dos: \n");
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = 0; j < size; j++)
+        {
             scanf("%d", &iMatTwo[i][j]);
         }
     }
-
-    printMatrices(iMatOne);
-    printMatrices(iMatTwo);
+    
+    //Llamada a funciones 
+    printf("---Matriz Uno---\n");
+    printMat(iMatOne);
+    printf("---Matriz Dos---\n");
+    printMat(iMatTwo);
+    SumaMat(iMatOne, iMatTwo); 
+    RestaMat(iMatOne, iMatTwo);
+    MultMat(iMatOne, iMatTwo);
 
 
     return 0;
