8d9f2d1d564b5f55c6c94b6c5b9f5b3309ec1c0f
David <davidcantumartinez@gmail.com>
2020-02-19 14:23:19 -0600

    Finished Tarea 5

---
+++ Code/Structs.c
@@ -0,0 +1,46 @@
+//Autor: David Alonso Cantú Martínez
+//Matrícula: A00822455
+//Fecha: 18/02/2020
+//Pide al usuario datos de su agente y los guarada en una estructura
+
+#include "stdio.h"
+#include "string.h"
+
+//Estructura usada para guardar los datos
+struct Agent{
+    char cNombre[30];
+    char cUNombre[30];
+    int iEdad;
+    char cGenero[12];
+    char cMision[25];
+};
+
+int main()
+{
+    struct Agent P;
+    char buf[10]; //Variable temporal
+    printf("%s\n", "Introduce el nombre del agente: ");
+    fgets(P.cNombre,30,stdin);
+
+    printf("%s\n", "Introduce el apellido del agente: ");
+    fgets(P.cUNombre,30,stdin);
+
+    printf("%s\n", "Introduce la edad del agente: ");
+    fgets(buf,10,stdin);
+
+    printf("%s\n", "Introduce el genero del agente: ");
+    fgets(P.cGenero,12,stdin);
+
+    printf("%s\n", "Introduce el id de la mision: ");
+    fgets(P.cMision,25,stdin);
+
+    P.iEdad = atoi(buf); //Convirtiendo String a Int
+    //Imprimiendo al Agente
+    printf("%s\n", "Agente: ");
+    printf("%s", A.cNombre);
+    printf("%s", A.cUNombre);
+    printf("%i\n", A.iEdad);
+    printf("%s", A.cGenero);
+    printf("%s", A.cMision);
+
+}
