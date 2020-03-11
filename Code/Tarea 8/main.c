76abc2084af8bad74c018253ce0f492d06e45a13
David <davidcantumartinez@gmail.com>
2020-03-11 10:59:35 -0600

    Finished practice 8

--- Code/Tarea 8/main.c
+++ Code/Tarea 8/main.c
@@ -11,7 +11,7 @@ union Valores
 {
     int i;
     double d;
-    char str[100];
+    char str[101];
 };
 
 
@@ -25,5 +25,15 @@ int main()
     scanf("%d", &datos.i);
     printf("Your integer %d storage size is %d bytes.\n", datos.i, sizeof(datos.i));
 
+    printf("Enter a double: ");
+    scanf("%lf", &datos.d);
+    printf("Your double %lf storage size is %d bytes, I can read any number from %lf to %lf in this data type \n", 
+    datos.d, (int)sizeof(datos.d), DBL_MIN, DBL_MAX);
+    
+    printf("Enter a string: ");
+    scanf("%100s", &datos.str);
+    printf("Your string %s storage size is %d bytes.\n", datos.str, sizeof(datos.str));
+
+  
     return 0;
 }
