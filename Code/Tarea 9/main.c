16efda20296b8bfa56e70c4027ecf2e83a5a7f54
David <davidcantumartinez@gmail.com>
2020-03-11 11:52:44 -0600

    Now it converts from hex to decimal

--- Code/Tarea 9/main.c
+++ Code/Tarea 9/main.c
@@ -6,6 +6,7 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+//función para convertir Binario a entero
 int asciiBinaryToInt(char *cBin)
 {
     char cFinal = '\0';
@@ -22,8 +23,55 @@ int asciiBinaryToInt(char *cBin)
     return iConvertido;
 }
 
+//función para convertit hexadecimal a entero
+int asciiHEXToInt(char *cHex)
+{
+    char cFinal = '\0';
+    int iConvertido = 0;
+    while(cFinal != *cHex)
+    {
+        iConvertido *= 16;
+        if(*cHex <= '9' && *cHex >= '0') 
+        {
+            iConvertido += *cHex - '0';
+        }
 
+        if(*cHex == 'A') 
+        {
+            iConvertido+=10;
+        }
 
+        if(*cHex == 'B') 
+        {
+            iConvertido+=11;
+        }
+
+        if(*cHex == 'C') 
+        {
+            iConvertido+=12;
+        }
+
+        if(*cHex == 'D') 
+        {
+            iConvertido+=13;
+        }
+
+        if(*cHex == 'E') 
+        {
+            iConvertido+=14;
+        }
+        
+        if(*cHex == 'F') 
+        {
+            iConvertido+=15;
+        }
+
+        cHex++;
+    }
+    return iConvertido;
+}
+
+//función principal 
 int main()
 {
 
@@ -32,6 +80,11 @@ int main()
     scanf("%s", cString);
     int iBinarioAInt = asciiBinaryToInt(cString);
     printf("%i \n", iBinarioAInt);
+
+    printf("%s", "Introduce un numero en hex: ");
+    scanf("%s", cString);
+    int iHexAInt = asciiHEXToInt(cString);
+    printf("%i \n", iHexAInt);
 
 
 
