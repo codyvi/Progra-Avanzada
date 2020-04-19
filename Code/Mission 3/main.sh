4af523843f174fb80f178bcab95d3322fd8f608c
David <davidcantumartinez@gmail.com>
2020-04-19 13:34:31 -0500

    Finished Mission 3

--- Code/Mission 3/main.sh
+++ Code/Mission 3/main.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-#Grep para buscar los hashtags del archivo html, los junta y devuelve en pantalla
-grep -o "hashtag\/[A-Za-z0-9_]\+" met.html | sort -n | uniq -ic 
+#grep to get the hashtags
+grep -o "hashtag\/[A-Za-z0-9_]\+" smith.html | sort -n | uniq -ic $1 > html_hashtags.txt
+#remove the hashtag and \ from the txt
+cat $"html_hashtags.txt" | sed "s/hashtag/ /" |  sed "s/\// /g" > $"text_hashtags.txt"
+#print in screen the hashtags
+cat text_hashtags.txt
