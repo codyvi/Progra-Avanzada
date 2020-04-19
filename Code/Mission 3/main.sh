22ff62cf4cb0a44ccb7ae6ada8af0b6b001adc23
David <davidcantumartinez@gmail.com>
2020-04-19 13:36:04 -0500

    Updated spaces in shell terminal output

--- Code/Mission 3/main.sh
+++ Code/Mission 3/main.sh
@@ -3,6 +3,6 @@
 #grep to get the hashtags
 grep -o "hashtag\/[A-Za-z0-9_]\+" smith.html | sort -n | uniq -ic $1 > html_hashtags.txt
 #remove the hashtag and \ from the txt
-cat $"html_hashtags.txt" | sed "s/hashtag/ /" |  sed "s/\// /g" > $"text_hashtags.txt"
+cat $"html_hashtags.txt" | sed "s/hashtag//" |  sed "s/\///g" > $"text_hashtags.txt"
 #print in screen the hashtags
 cat text_hashtags.txt
